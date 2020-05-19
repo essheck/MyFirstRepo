50c82d5acf92f78712845f5735c0bc58dd557139
Shivam Singh <shivam.stan758@gmail.com>
2020-05-19 17:07:04 +0530

    add new about file in views and add the route to app file

--- app.js
+++ app.js
@@ -4,9 +4,20 @@ var express = require("express"),
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+//Routes
+
+//HOME
 app.get("/" , function(req ,res){
     res.render("home.ejs");
 });
+
+//About
+app.get("/about", function(req, res){
+    res.render("about.ejs");
+});
+
+
 
     
 app.listen(8080 , function(req ,res){
